0da7fefa6713bbc7b07db30af378ef3dff323bf9
JolyHuang <JolyEmail@Gmail.COM>
2018-07-05 15:06:12 +0800

    init

--- blockchain-server/src/main/java/com/sharingif/blockchain/app/autoconfigure/persistence/datasource/PersistenceComponentsAutoconfigure.java
+++ blockchain-server/src/main/java/com/sharingif/blockchain/app/autoconfigure/persistence/datasource/PersistenceComponentsAutoconfigure.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @Configuration
@@ -40,7 +41,7 @@ public class PersistenceComponentsAutoconfigure {
 	}
 
 	@Bean(name="dataSource")
-	public org.apache.tomcat.jdbc.pool.DataSource createDataSource(DataSourcePoolConfig dataSourcePoolConfig, TextEncryptor propertyTextEncryptor) throws SQLException {
+	public DataSource createDataSource(DataSourcePoolConfig dataSourcePoolConfig, TextEncryptor propertyTextEncryptor) throws SQLException {
 		PoolProperties poolProperties = new PoolProperties();
 		poolProperties.setJdbcInterceptors("org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
 
