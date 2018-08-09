f6737f98eccd66961b72d6e7e3c45f6a88d6542c
JolyHuang <JolyEmail@Gmail.COM>
2018-08-09 19:20:43 +0800

    init

--- account-batch/src/main/java/com/sharingif/blockchain/eth/service/impl/WithdrawalUntreatedStatusEthServiceImpl.java
+++ account-batch/src/main/java/com/sharingif/blockchain/eth/service/impl/WithdrawalUntreatedStatusEthServiceImpl.java
@@ -35,7 +35,7 @@ import java.util.concurrent.TimeUnit;
  * @since v1.0
  * 2018/7/25 下午6:01
  */
-//@Service
+@Service
 public class WithdrawalUntreatedStatusEthServiceImpl implements InitializingBean {
 
     protected final Logger logger = LoggerFactory.getLogger(getClass());
