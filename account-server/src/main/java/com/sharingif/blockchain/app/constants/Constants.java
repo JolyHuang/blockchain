2dd1ca36d1f39511acf5b43b1e209cfaba553736
JolyHuang <JolyEmail@Gmail.COM>
2018-08-13 11:22:08 +0800

    优化

---
+++ account-server/src/main/java/com/sharingif/blockchain/app/constants/Constants.java
@@ -0,0 +1,18 @@
+package com.sharingif.blockchain.app.constants;
+
+/**
+ * Constants
+ *
+ * @author Joly
+ * @version v1.0
+ * @since v1.0
+ * 2018/4/24 下午2:47
+ */
+public class Constants {
+
+    /**
+     * btc网络类型(test:测试网络)
+     */
+    public static final String BTC_NET_TYPE_TEST = "test";
+
+}
